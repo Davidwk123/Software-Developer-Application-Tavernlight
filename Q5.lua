94bb56663eacc0af9c8f9a20475c2e5eb86ab7ed
David W.K <dwelchkeliihoomalu@gmail.com>
2024-05-14 13:13:00 -1000

    Question 5
    Question 5 with video

---
+++ Q5.lua
@@ -0,0 +1,19 @@
+-- I copied the Eternal Winter spell and changed some parameters to better fit question 5
+-- This is the file that I added into the server code
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+-- I changed the area of the spell to the preset AREA_CIRCLE3X3
+combat:setArea(createCombatArea(AREA_CIRCLE3X3))
+
+function onGetFormulaValues(player, level, magicLevel)
+	local min = (level / 5) + (magicLevel * 5.5) + 25
+	local max = (level / 5) + (magicLevel * 11) + 50
+	return -min, -max
+end
+
+combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+
+function onCastSpell(creature, variant)
+	return combat:execute(creature, variant)
+end
